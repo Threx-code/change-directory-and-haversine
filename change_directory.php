d4a30d9af26c6828c981d5352e2b07cbc102389c
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2023-08-30 08:41:02 +0100

    completed logic pushed

--- change_directory.php
+++ change_directory.php
@@ -4,7 +4,9 @@ namespace App;
 
 class ChangeDirectory
 {
-    public $currentPath;
+    public mixed $currentPath;
+    const  SEPARATOR = '/';
+    const PATH_TO_CHECK = ['..', '.'];
 
     function __construct($path = 'a/b/c/d')
     {
@@ -12,59 +14,49 @@ class ChangeDirectory
     }
 
     /**
-     * @param $newPath
+     * @param string|null $newPath
      * @return $this|void
      */
-    public function cd($newPath)
+    public function cd(?string $newPath)
     {
-        $separator = '/';
-        $pathToCheck = ['..', '.'];
-        if (substr($newPath, 0, 1) === '/') {
+        $pathToCheck = self::PATH_TO_CHECK;
+        if (str_starts_with($newPath, '/')) {
             $this->currentPath = $newPath;
             return;
         }
 
-        $currentPathCount = count( $this->convertToArray($this->currentPath, $separator) );
+        $currentPathCount = count( $this->convertToArray($this->currentPath) );
         $newPathCount = substr_count($newPath, '..' !== null ? '..' : '.');
-        $currentDirectories = array_filter($this->convertToArray($this->currentPath, $separator));
+        $currentDirectories = array_filter($this->convertToArray($this->currentPath));
 
-        $newDirectories = array_filter( $this->convertToArray($newPath, $separator), function($value) use($pathToCheck) {
+        $newDirectories = array_filter( $this->convertToArray($newPath), function($value) use($pathToCheck) {
             return !empty($value) && !in_array($value, $pathToCheck, true);
         });
 
         $newPaths = array_slice($currentDirectories, 0,  $newPathCount > $currentPathCount ? 0 : $currentPathCount - $newPathCount );
         array_push($newPaths, ...$newDirectories);
-        $this->currentPath =  $this->implodeData($newPaths, $separator);
+        $this->currentPath =  $this->implodeData($newPaths);
         return $this;
 
     }
 
     /**
-     * @param $data
-     * @param $separator
-     * @return false|mixed|string[]
+     * @param string $data
+     * @return string[]
      */
-    private function convertToArray($data, $separator)
+    private function convertToArray(string $data): array
     {
-        $toArray = $data;
-        if(!is_array($toArray)){
-            $toArray = explode($separator, $toArray);
-        }
-        return $toArray;
+        return explode(self::SEPARATOR, $data);
+
     }
 
     /**
-     * @param $data
-     * @param $separator
+     * @param array $data
      * @return string
      */
-    private function implodeData($data, $separator)
+    private function implodeData(array $data): string
     {
-        $toArray = $data;
-        if(!is_array($toArray)){
-            $toArray = explode($separator, $toArray);
-        }
-        return implode($separator, $toArray);
+        return implode(self::SEPARATOR, $data);
     }
     
 }
